aaa706f4bcaee23dba8a1875b60c1b8b459affbf
Chidera Aniekwe <chideraaniekwederah73@gmail.com>
2024-08-18 20:55:37 +0100

    Recent update

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '/public')));
+
 // Root route to render index.ejs
 app.get('/', (req, res) => {
   res.render('index.ejs');
